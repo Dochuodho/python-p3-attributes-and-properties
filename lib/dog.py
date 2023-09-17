9869884891594beb0c70c79ef6d676ed0628047a
Ochuodho <ochuodhoderrick@gmail.com>
2023-09-17 17:02:01 +0300

    Incomplete

--- lib/dog.py
+++ lib/dog.py
@@ -11,5 +11,28 @@ APPROVED_BREEDS = [
     "Pointer"
 ]
 
+
 class Dog:
-    pass
+    def __init__(self, name):
+        self._name = name
+
+    def get_name(self):
+        print("Retrieving name")
+        return self._name
+    
+    def set_name(self, name):
+        if(type(name) in (str)) and (1 <= len(name) <= 24):
+            print(f"Setting name {name}")
+            self._name = name
+
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    name = property(get_name, set_name)
+fido = Dog("Fido")
+fido.name = "Fido"
+
+
+
+    
+pass
